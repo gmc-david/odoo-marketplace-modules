18b0e6ed1367415e9ed8c07a4c521f83cc7e34ef
gmc-david <david@thegreatmerch.com>
2026-03-14 13:27:10 +0000

    Update __manifest__.py

--- tgm_website_encouraging_price/__manifest__.py
+++ tgm_website_encouraging_price/__manifest__.py
@@ -28,8 +28,8 @@ Perfect for B2B stores, wholesale operations, or any business offering volume di
     "author": "The Great Merch Developers",
     "website": "https://thegreatmerch.com",
     "support": "hello@thegreatmerch.com",
-    "price": 19.99,
-    "currency": "GBP",
+    "price": 23.99,
+    "currency": "EUR",
     "license": "LGPL-3",
     "images": [
         "static/description/banner.png",
